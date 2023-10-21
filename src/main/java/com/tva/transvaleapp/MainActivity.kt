e554a6770264f992d57aa6a63c8515a1e8f06a96
Vitor.Dev <vitorfeliperamosmello8@gmail.com>
2023-10-21 13:51:07 -0300

    About Btn & Other Activity
    Created the "about" section btn and "About" activity to apply the intent

--- src/main/java/com/tva/transvaleapp/MainActivity.kt
+++ src/main/java/com/tva/transvaleapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.tva.transvaleapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val aboutBtn: Button = findViewById(R.id.aboutBtn)
+
+        aboutBtn.setOnClickListener(){
+            val intent = Intent(this, About::class.java)
+            startActivity(intent)
+        }
     }
 }
